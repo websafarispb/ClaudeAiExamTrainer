ca08d49844020c73fd105a185a145d0a21cf9092
Evgenii_Stepanov2 <evgenii_stepanov2@epam.com>
2026-05-27 17:08:22 +0300

    Add database healthcheck to Docker Compose

--- backend/src/main/java/com/exam/aiexamtrainer/service/impl/AiQuestionGenerationServiceImpl.java
+++ backend/src/main/java/com/exam/aiexamtrainer/service/impl/AiQuestionGenerationServiceImpl.java
@@ -149,8 +149,8 @@ public class AiQuestionGenerationServiceImpl implements AiQuestionGenerationServ
     String rawResponse = client.generate(systemPrompt, userPrompt);
 
     try {
-      log.info("RAW PRACTICAL TASK TRANSLATION RESPONSE:");
-      log.info(rawResponse);
+      log.debug("RAW PRACTICAL TASK TRANSLATION RESPONSE:");
+      log.debug(rawResponse);
       TranslatePracticalTaskResponseDto response =
           objectMapper.readValue(rawResponse, TranslatePracticalTaskResponseDto.class);
 
